01de465a6b9deb0126e4d4f06f82b503e7916f35
Bogdan Ponomarenko <ponomarenko.bogdan@yandex.ru>
2026-03-07 22:40:42 +0300

    feat: create reusable card

---
+++ frontend/components/Card.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import type { FC } from 'react';
+import styles from './Card.css';
+
+type Props = {};
+
+export const Card: FC<Props> = (props) => (
+    <div></div>
+);
